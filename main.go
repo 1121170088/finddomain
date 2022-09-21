dfba752f16526807baf310b4d016f3c0d7e8aa9f
tanmingxin <112117088@qq.com>
2022-09-21 18:14:01 +0800

    update

--- main.go
+++ main.go
@@ -7,12 +7,11 @@ import (
 	"errors"
 	"flag"
 	"github.com/babolivier/go-doh-client"
-	"github.com/oschwald/geoip2-golang"
+	cidranger "github.com/yl2chen/cidranger"
 	"gopkg.in/yaml.v3"
 	"io"
 	"io/ioutil"
 	"log"
-	"net"
 	"net/http"
 	"net/url"
 	"os"
@@ -21,7 +20,6 @@ import (
 	"regexp"
 	"syscall"
 	"time"
-	cidranger "github.com/yl2chen/cidranger"
 )
 
 var (
@@ -31,10 +29,11 @@ var (
 	config Config
     Tree map[byte] *node = make(map[byte] *node)
     domainFile = "list.txt"
-    mmdbFile = "GeoLite2-Country.mmdb"
     domainFilePtr *os.File
-    mmdbFilePtr *geoip2.Reader
 	ranger cidranger.Ranger
+    parseQueryLog bool
+    queryLog string
+    domainRegex *regexp.Regexp
 
 )
 
@@ -44,7 +43,8 @@ type Config struct {
 	Authorization string          `yaml:"authorization"`
 	StartQueryTime string        `yaml:"start-query-time"`
 	Codes       []string           `yaml:"codes"`
-	CustomIpList string           `yaml:"custom-ip-list"`
+	IpInfoServer string             `yaml:"ip-info-server"`
+	DomainRegex string         `yaml:"domain-regex"`
 
 }
 type Question struct {
@@ -63,22 +63,22 @@ type node struct{
 	folw map[byte] *node
 }
 
-type payload struct {
-	Payload     []string  `yaml:"payload"`
-}
-
 func init() {
 	flag.StringVar(&homeDir, "d", "./", "set configuration directory")
+	flag.StringVar(&domainFile, "o", "list.txt", "set output file name under -d")
+	flag.BoolVar(&parseQueryLog, "p", false, "parse anduard home log file mode, default false")
+	flag.StringVar(&queryLog, "l", "", "adguard home query log file")
 	flag.Parse()
 }
 
 func myInit()  {
 	config = Config{
-		Doh: "223.5.5.5",
+		Doh: "dns-unfiltered.adguard.com",
 		AdguardHome: "",
 		Authorization: "xx:xx",
 		StartQueryTime: "2022-09-01T00:00:00+08:00",
-		CustomIpList: "",
+		IpInfoServer: "",
+		DomainRegex: `[^-.][-a-zA-Z0-9]+\.(ac|academy|ac\.cn|accountant|accountants|actor|ad|adult|ae|aero|af|ag|agency|ah\.cn|ai|airforce|al|am|amsterdam|an|ao|apartments|app|aq|ar|archi|army|art|as|asia|associates|at|attorney|au|auction|auto|autos|aw|az|ba|baby|band|bar|barcelona|bargains|bayern|bb|bd|be|beauty|beer|berlin|best|bet|bf|bg|bh|bi|bid|bike|bingo|bio|biz|biz\.pl|bj|bj\.cn|black|blog|blue|bm|bn|bo|boats|boston|boutique|br|bs|bt|build|builders|business|buzz|bv|bw|by|bz|ca|cab|cafe|camera|camp|capital|car|cards|care|careers|cars|casa|cash|casino|catering|cc|cd|center|ceo|cf|cg|ch|charity|chat|cheap|church|ci|city|ck|cl|claims|cleaning|clinic|clothing|cloud|club|cm|cn|co|coach|codes|coffee|co\.in|co\.jp|co\.kr|college|com|com\.ag|com\.au|com\.br|com\.bz|com\.cn|com\.co|com\.es|com\.ky|community|com\.mx|company|com\.pe|com\.ph|com\.pl|computer|com\.tw|condos|construction|consulting|contact|contractors|co\.nz|cooking|cool|coop|co\.uk|country|coupons|courses|co\.za|cq\.cn|cr|credit|creditcard|cricket|cruises|cu|cv|cx|cy|cymru|cz|dance|date|dating|de|deals|degree|delivery|democrat|dental|dentist|design|dev|diamonds|digital|direct|directory|discount|dj|dk|dm|do|doctor|dog|domains|download|dz|earth|ec|edu|education|ee|eg|eh|email|energy|engineer|engineering|enterprises|equipment|er|es|estate|et|eu|events|exchange|expert|exposed|express|fail|faith|family|fan|fans|farm|fashion|fi|film|finance|financial|firm\.in|fish|fishing|fit|fitness|fj|fj\.cn|fk|flights|florist|fm|fo|football|forsale|foundation|fr|fun|fund|furniture|futbol|fyi|ga|gallery|games|garden|gay|gd|gd\.cn|ge|gen\.in|gf|gg|gh|gi|gifts|gives|gl|glass|global|gm|gmbh|gn|gold|golf|gov|gov\.cn|gp|gq|gr|graphics|gratis|green|gripe|group|gs|gs\.cn|gt|gu|guide|guru|gw|gx\.cn|gy|gz\.cn|ha\.cn|hair|haus|hb\.cn|health|healthcare|he\.cn|hi\.cn|hk|hk\.cn|hl\.cn|hm|hn|hn\.cn|hockey|holdings|holiday|homes|horse|hospital|host|house|hr|ht|hu|id|idv|idv\.tw|ie|il|im|immo|immobilien|in|inc|ind\.in|industries|info|info\.pl|ink|institute|insure|int|international|investments|io|iq|ir|irish|is|ist|istanbul|it|je|jetzt|jewelry|jl\.cn|jm|jo|jobs|jp|js\.cn|jx\.cn|kaufen|ke|kg|kh|ki|kim|kitchen|kiwi|km|kn|kp|kr|kw|ky|kz|la|land|law|lawyer|lb|lc|lease|legal|lgbt|li|life|lighting|limited|limo|live|lk|llc|ln\.cn|loan|loans|london|love|lr|ls|lt|ltd|ltda|lu|luxury|lv|ly|ma|maison|makeup|management|market|marketing|mba|mc|md|me|media|melbourne|memorial|men|menu|me\.uk|mg|mh|miami|mil|mk|ml|mm|mn|mo|mobi|mo\.cn|moda|moe|money|monster|mortgage|motorcycles|movie|mp|mq|mr|ms|mt|mu|museum|mv|mw|mx|my|mz|na|nagoya|name|navy|nc|ne|ne\.kr|net|net\.ag|net\.au|net\.br|net\.bz|net\.cn|net\.co|net\.in|net\.ky|net\.nz|net\.pe|net\.ph|net\.pl|network|news|nf|ng|ni|ninja|nl|nm\.cn|no|nom\.co|nom\.es|nom\.pe|np|nr|nrw|nu|nx\.cn|nyc|nz|okinawa|om|one|onl|online|org|org\.ag|org\.au|org\.cn|org\.es|org\.in|org\.ky|org\.nz|org\.pe|org\.ph|org\.pl|org\.uk|pa|page|paris|partners|parts|party|pe|pet|pf|pg|ph|photography|photos|pictures|pink|pizza|pk|pl|place|plumbing|plus|pm|pn|poker|porn|pr|press|pro|productions|promo|properties|protection|ps|pt|pub|pw|py|qa|quebec|quest|racing|re|realestate|recipes|red|rehab|reise|reisen|re\.kr|rent|rentals|repair|report|republican|rest|restaurant|review|reviews|rich|rip|ro|rocks|rodeo|ru|run|rw|ryukyu|sa|sale|salon|sarl|sb|sc|school|schule|science|sd|se|security|services|sex|sg|sh|shiksha|shoes|shop|shopping|show|si|singles|site|sj|sk|ski|skin|sl|sm|sn|so|soccer|social|software|solar|solutions|space|/span|sr|st|storage|store|stream|studio|study|style|supplies|supply|support|surf|surgery|sv|sy|sydney|systems|sz|tax|taxi|tc|td|team|tech|technology|tel|tennis|tf|tg|th|theater|theatre|tienda|tips|tires|tj|tk|tl|tm|tn|to|today|tokyo|tools|tours|town|toys|tp|tr|trade|training|travel|tt|tube|tv|tw|tz|ua|ug|uk|um|university|uno|us|uy|uz|va|vacations|vc|ve|vegas|ventures|vet|vg|vi|viajes|video|villas|vin|vip|vision|vn|vodka|vote|voto|voyage|vu|wales|watch|webcam|website|wedding|wf|wiki|win|wine|work|works|world|ws|wtf|xxx|xyz|yachts|ye|yoga|yokohama|yr|yt|yu|za|zm|zone|zw|移动)$`,
 
 	}
 	configFile := filepath.Join(homeDir, configfile)
@@ -93,6 +93,7 @@ func myInit()  {
 				log.Printf(err.Error())
 			}
 		}
+		os.Exit(1)
 	} else {
 		if bytes, err := os.ReadFile(configFile); err == nil {
 			if err = yaml.Unmarshal(bytes, &config); err != nil {
@@ -109,7 +110,6 @@ func myInit()  {
 	}
 
 	domainFile = filepath.Join(homeDir, domainFile)
-	mmdbFile = filepath.Join(homeDir, mmdbFile)
 	var err error
 	domainFilePtr, err = OpenFile(domainFile)
 	if err != nil {
@@ -122,58 +122,14 @@ func myInit()  {
 		if err != nil || err == io.EOF {
 			break
 		}
-		//firstSlash := strings.Index(line, "/")
-		//if firstSlash == -1 {
-		//	continue
-		//}
-		//firstSlash += 1
-		//secondSlash := strings.Index(line[firstSlash:], "/")
-		//if secondSlash == -1 {
-		//	continue
-		//}
-
-		//domain := line[firstSlash: firstSlash + secondSlash]
 		hasDomain(line)
 	}
 
-	mmdbFilePtr, err = geoip2.Open(mmdbFile)
-	if err != nil {
-		log.Fatal(err)
-	}
-
-	ranger = cidranger.NewPCTrieRanger()
-	if config.CustomIpList != "" {
-		str, err := httpGet(config.CustomIpList)
-		if err != nil {
-			log.Printf(err.Error())
-		}
-		rawCfg := &payload{}
-		if err := yaml.Unmarshal([]byte(str), rawCfg); err != nil {
-			log.Printf(err.Error())
-		}
-		for _ , ele := range rawCfg.Payload {
-			_, network2, _ := net.ParseCIDR(ele)
-			ranger.Insert(cidranger.NewBasicRangerEntry(*network2))
-		}
-		log.Printf("%s loaded %d lines", "customip", len(rawCfg.Payload))
-	}
+	domainRegex = regexp.MustCompile(config.DomainRegex)
 
 }
 
-func httpGet(url string) (string, error) {
-	response, err := http.Get(url)
-	if err != nil {
-		log.Printf("get error")
-	}
-	defer response.Body.Close()
-	body, err2 := ioutil.ReadAll(response.Body)
-	if err2 != nil {
-		log.Printf("ioutil read error")
-	}
-	return string(body), err
-}
-
-func lookup(domain string) (ip string, err error)  {
+func lookup2(domain string) (ip string, err error)  {
 	a, _, err := resolver.LookupA(domain)
 	if err != nil {
 		return "", err
@@ -290,41 +246,16 @@ func do()  {
 			}
 			for _, e := range adlog.Data {
 				domain := e.Question.Name
-				if !isDomain(domain){
-					log.Printf(" %s is not a domain, skipping it", domain)
+				shortDomain := searchDomain(domain)
+				if !isDomain(shortDomain){
+					log.Printf(" %s is not a domain   %s", shortDomain, domain)
 					continue
 				}
-				if !hasDomain(domain) {
-					if ipstr, err := lookup(domain); err == nil {
-						ip := net.ParseIP(ipstr)
 
-						var contain = false
-						if contains, _ := ranger.Contains(ip); contains {
-							log.Printf("%s 's ip matches custom ip list", domain)
-							contain = true
-						} else if country, err := mmdbFilePtr.Country(ip); err == nil {
-							code := country.Country.IsoCode
-							for _, c := range config.Codes {
-								if c == code {
-									log.Printf("%s 's ip in codes list", domain)
-									contain = true
-									break
-								}
-							}
-						}
-						if contain {
-							if _, err := io.WriteString(domainFilePtr, domain +"\n"); err != nil {
-								log.Fatal(err.Error())
-							}
-
-						} else {
-							log.Printf("%s 's ip not in codes list or custom ip list", domain)
-						}
-
-					} else {
-						log.Printf("%s dosn't have ip", domain)
+				if !hasDomain(shortDomain) {
+					if _, err := io.WriteString(domainFilePtr, shortDomain +"\n"); err != nil {
+						log.Fatal(err.Error())
 					}
-
 				} else  {
 					log.Printf("%s has cached", domain)
 				}
@@ -341,20 +272,83 @@ func isDomain(domain string) bool  {
 	return  match1 && match2 && []byte(domain)[0] != '-'
 
 }
+func IpCode(ip string) string {
+	response, err := http.Get(config.IpInfoServer + "?" + ip)
+	if err != nil {
+		log.Printf(err.Error())
+		return ""
+	}
+	defer response.Body.Close()
+	body, err2 := ioutil.ReadAll(response.Body)
+	if err2 != nil {
+		log.Printf(err2.Error())
+	}
+	jsonV := struct {
+		Code string
+	}{}
+	err = json.Unmarshal(body, &jsonV)
+	if err != nil {
+		log.Fatal(err.Error())
+		return ""
+	}
+	return jsonV.Code
+}
+
+func parseLog()  {
+	var err error
+	f, err := OpenFile(queryLog)
+	if err != nil {
+		log.Fatal(err)
+	}
+	rd := bufio.NewReader(f)
+
+	for {
+		line, err := rd.ReadString('\n')
+		if err != nil || err == io.EOF {
+			break
+		}
+		var dm = struct {
+			QH string
+		}{}
+		err = json.Unmarshal([]byte(line), &dm)
+		if err != nil {
+			log.Printf(err.Error())
+			continue
+		}
+		domain := dm.QH
+		shortDomain := searchDomain(domain)
+		if !isDomain(shortDomain){
+			log.Printf(" %s is not a domain   %s", shortDomain, domain)
+			continue
+		}
+
+		if !hasDomain(shortDomain) {
+			if _, err := io.WriteString(domainFilePtr, shortDomain +"\n"); err != nil {
+				log.Fatal(err.Error())
+			}
+		} else  {
+			log.Printf("%s has cached", domain)
+		}
+	}
+}
+func searchDomain(domain string) string  {
+	return domainRegex.FindString(domain)
+}
 func main() {
 
 	myInit()
-	go do()
 
-	sigCh := make(chan os.Signal, 1)
-	signal.Notify(sigCh, syscall.SIGINT, syscall.SIGTERM)
-	<-sigCh
+	if !parseQueryLog {
+		go do()
+		sigCh := make(chan os.Signal, 1)
+		signal.Notify(sigCh, syscall.SIGINT, syscall.SIGTERM)
+		<-sigCh
+	} else {
+		parseLog()
+	}
 
 	if domainFilePtr != nil {
 		domainFilePtr.Close()
-	}
-	if mmdbFilePtr != nil {
-		mmdbFilePtr.Close()
 	}
 
 	if bytes, err :=yaml.Marshal(config); err == nil {
@@ -362,7 +356,6 @@ func main() {
 		if err != nil {
 			log.Fatal(err)
 		}
-
 	}
 
 }
